9ddd88a9f6a1f94bbabfe1f7025c9e37361854b0
thanhhaa <hathanh1095@gmail.com>
2023-02-08 23:04:49 +0700

    Done Set2b exe

---
+++ hmf/Set2b.hs
@@ -0,0 +1,223 @@
+module Set2b where
+
+import Mooc.Todo
+
+-- Some imports you'll need. Don't add other imports :)
+import Data.List
+
+------------------------------------------------------------------------------
+-- Ex 1: compute binomial coefficients using recursion. Binomial
+-- coefficients are defined by the following equations:
+--
+--   B(n,k) = B(n-1,k) + B(n-1,k-1)
+--   B(n,0) = 1
+--   B(0,k) = 0, when k>0
+--
+-- Hint! pattern matching is your friend.
+
+binomial :: Integer -> Integer -> Integer
+binomial  n k
+   | k == 0          = 1
+   | n == 0 && k > 0 = 0
+   |otherwise        = binomial (n-1) k + binomial (n-1) (k-1)
+
+-- Model solution
+-- binomial :: Integer -> Integer -> Integer
+-- binomial n 0 = 1
+-- binomial 0 k = 0
+-- binomial n k = binomial (n-1) k + binomial (n-1) (k-1)
+
+------------------------------------------------------------------------------
+-- Ex 2: implement the odd factorial function. Odd factorial is like
+-- factorial, but it only multiplies odd numbers.
+--
+-- Examples:
+--   oddFactorial 7 ==> 7*5*3*1 ==> 105
+--   oddFactorial 6 ==> 5*3*1 ==> 15
+
+oddFactorial :: Integer -> Integer
+oddFactorial n
+   | n < 0 || n == 0 = 0
+   | n == 1          = 1
+   | n == 2          = 1
+   | otherwise       = nNew * oddFactorial(nNew - 2)
+   where nNew = if odd n then n else n-1
+
+-- Model solution
+-- oddFactorial :: Integer -> Integer
+-- oddFactorial 1 = 1
+-- oddFactorial n = if even n then oddFactorial (n-1) else n * oddFactorial (n-1)
+
+------------------------------------------------------------------------------
+-- Ex 3: implement the Euclidean Algorithm for finding the greatest
+-- common divisor:
+--
+-- Given two numbers, a and b,
+-- * if one is zero, return the other number
+-- * if not, subtract the smaller number from the larger one
+-- * replace the larger number with this new number
+-- * repeat
+--
+-- For example,
+--   myGcd 9 12 ==> 3
+-- In this case, the algorithm proceeds like this
+--
+--   a      b
+--
+--   9      12
+--   9      (12-9)
+--   9      3
+--   (9-3)  3
+--   6      3
+--   (6-3)  3
+--   3      3
+--   (3-3)  3
+--   0      3
+--
+-- Background reading:
+-- * https://en.wikipedia.org/wiki/Euclidean_algorithm
+
+myGcd :: Integer -> Integer -> Integer
+myGcd a b
+   | b == 0    = a
+   | a == 0    = b
+   | a > b     = myGcd (a-b) b
+   | otherwise = myGcd a (b-a)
+
+-- Model solution
+-- myGcd :: Integer -> Integer -> Integer
+-- myGcd 0 y = y
+-- myGcd x y = if xAbs < yAbs
+--             then myGcd yAbs xAbs
+--             else myGcd (xAbs-yAbs) yAbs
+--   -- Using the absolute values of x and y makes this function work
+--   -- even with negative inputs. This is not required for this set.
+--   -- However, without this fix you can run into problems in Set 6,
+--   -- if you reuse this answer :)
+--   where xAbs = abs x
+--         yAbs = abs y
+
+------------------------------------------------------------------------------
+-- Ex 4: Implement the function leftpad which adds space characters
+-- to the start of the string until it is long enough.
+--
+-- Examples:
+--   leftpad "foo" 5 ==> "  foo"
+--   leftpad "13" 3 ==> " 13"
+--   leftpad "xxxxx" 3 ==> "xxxxx"
+--
+-- Tips:
+-- * you can combine strings with the ++ operator.
+-- * you can compute the length of a string with the length function
+
+leftpad :: String -> Int -> String
+leftpad str n
+   | length str >= n  = str
+   | otherwise        = leftpad (" " ++ str) n
+
+-- Model solution
+-- leftpad :: String -> Int -> String
+-- leftpad s i = if length s >= i then s else leftpad (" "++s) i
+
+------------------------------------------------------------------------------
+-- Ex 5: let's make a countdown for a rocket! Given a number, you
+-- should produce a string that says "Ready!", counts down from the
+-- number, and then says "Liftoff!".
+--
+-- For example,
+--   countdown 4 ==> "Ready! 4... 3... 2... 1... Liftoff!"
+--
+-- Hints:
+-- * you can combine strings with the ++ operator
+-- * you can use the show function to convert a number into a string
+-- * you'll probably need a recursive helper function
+
+countdown :: Integer -> String
+countdown n = countdownHelper n "Ready! "
+
+countdownHelper :: Integer -> String -> String
+countdownHelper n str
+   | n == 0    = str ++ "Liftoff!"
+   | otherwise  = countdownHelper (n - 1) (str ++ show n ++ "... ")
+
+-- Model solution
+-- countdown :: Integer -> String
+-- countdown n = "Ready!   " ++ helper n ++ "Liftoff!"
+
+-- helper 0 = ""
+-- helper n = show n ++ "... " ++ helper (n-1)
+
+------------------------------------------------------------------------------
+-- Ex 6: implement the function smallestDivisor that returns the
+-- smallest number (greater than 1) that divides the given number evenly.
+--
+-- That is, when
+--   smallestDivisor n ==> k
+-- we have
+--   n = t*k
+-- for some t.
+--
+-- Ps. your function doesn't need to work for inputs 0 and 1, but
+-- remember this in the next exercise!
+--
+-- Hint: remember the mod function!
+
+smallestDivisor :: Integer -> Integer
+smallestDivisor n = smallestDivisorHelper n (n-1)
+
+smallestDivisorHelper :: Integer -> Integer -> Integer
+smallestDivisorHelper n m
+   | n `mod` m == 0 = n `div` m
+   | otherwise      = smallestDivisorHelper n (m-1)
+
+-- Model solution
+-- smallestDivisor :: Integer -> Integer
+-- smallestDivisor n = smallestDivisor' 2 n
+
+-- smallestDivisor' k n =
+--   if mod n k == 0
+--   then k
+--   else smallestDivisor' (k+1) n
+
+------------------------------------------------------------------------------
+-- Ex 7: implement a function isPrime that checks if the given number
+-- is a prime number. Use the function smallestDivisor.
+--
+-- Ps. 0 and 1 are not prime numbers
+
+isPrime :: Integer -> Bool
+isPrime n
+   | n == 0 || n == 1   = False
+   | sD == n || sD == 1 = True
+   | otherwise          = False
+   where sD = smallestDivisor n
+
+-- Model solution
+-- isPrime :: Integer -> Bool
+-- isPrime 0 = False
+-- isPrime 1 = False
+-- isPrime i = smallestDivisor i == i
+
+------------------------------------------------------------------------------
+-- Ex 8: implement a function biggestPrimeAtMost that returns the
+-- biggest prime number that is less than or equal to the given
+-- number. Use the function isPrime you just defined.
+--
+-- You don't need to care about arguments less than 2. Any behaviour
+-- for them is fine.
+--
+-- Examples:
+--   biggestPrimeAtMost 3 ==> 3
+--   biggestPrimeAtMost 10 ==> 7
+
+biggestPrimeAtMost :: Integer -> Integer
+biggestPrimeAtMost n
+   | isPrime n   = n
+   | otherwise   = biggestPrimeAtMost (n-1)
+
+-- Model solution
+-- biggestPrimeAtMost :: Integer -> Integer
+-- biggestPrimeAtMost n =
+--   if isPrime n
+--   then n
+--   else biggestPrimeAtMost (n-1)
